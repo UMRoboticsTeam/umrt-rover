fae6fdcce5f3188e0c7405d4d99735982b4e407e
edcela <abanto.edcel@gmail.com>
2026-07-28 19:56:44 -0500

    Launch files

---
+++ launch/cameras.launch.py
@@ -0,0 +1,95 @@
+"""
+UMRT Robot Cameras Launch
+
+TO-DO:
+- Work on QoS Profile for all cameras
+- Define Launch Arguments for Cameras to keep a persistent way of getting /dev/video
+"""
+
+"""
+Imports
+"""
+from launch import LaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy 
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory  
+        
+"""
+Generate Launch Description 
+"""
+def generate_launch_description():
+
+
+    """
+    Parameters
+    """
+        
+    #   Rover Cameras
+    DeclareLaunchArgument(
+        "rover_cameras",
+        default_value="True",
+        description="Launch only the rover cameras.",
+    )
+
+    #   Arm Cameras
+    DeclareLaunchArgument(
+        "robotic_arm_cameras",
+        default_value="True",
+        description="Launch only the robotic arm cameras.",
+    )
+
+
+    """
+    Variables
+    """
+    rover_cams = LaunchConfiguration("rover_cameras")
+    arm_cams = LaunchConfiguration("robotic_arm_cameras")
+
+
+    """
+    Directories
+    """
+    serial_cam_launch_dir = os.path.join(
+            get_package_share_directory('umrt-serial-cam-ros'), 'launch'
+    )
+
+    poe_cam_launch_dir = os.path.join(
+            get_package_share_directory('umrt-ros-poe-cam'), 'launch'
+    )
+
+    """
+    External Launch Files
+    """
+    
+    poe_cam = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(poe_cam_launch_dir, 'mobile_publisher.launch.py')
+        )
+    )
+
+    compressed_conversion = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(poe_cam_launch_dir, 'transport.launch.py')
+        )
+    )
+
+    serial_cams = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(serial_cam_launch_dir, 'robot.launch.py')
+        )
+    )
+
+    """
+    Launch
+    """
+    vision = [
+        poe_cam,
+        compressed_conversion,
+        serial_cams
+    ]
+
+    return LaunchDescription(vision)
--- launch/rover.launch.py
+++ launch/rover.launch.py
@@ -5,13 +5,20 @@ UMRT Rover Launch File
 """
 Imports
 """
+import os
 from launch import LaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, RegisterEventHandler
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy 
 from launch_ros.actions import Node
-import os
+
 from ament_index_python.packages import get_package_share_directory
+from launch_ros.substitutions import FindPackageShare
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
+
 
 """
 Generate Launch Description
@@ -23,23 +30,54 @@ def generate_launch_description():
     """
     # Path to the drivetrain launch directory
     drivetrain_launch_dir = os.path.join(
-        get_package_share_directory('umrt-drivetrain-ros'), 'launch')
+        get_package_share_directory('umrt-arm-ros-firmware'), 'launch')
     
+    # Path to the GPS launch directory
+    gps_launch_dir = os.path.join(
+        get_package_share_directory('umrt-localization-ros'), 'launch')
+    
+    """
+    Launch Arguments
+    """
+    #   GUI
+    DeclareLaunchArgument(
+        "gui",
+        default_value="True",
+        description="Start RViz2 automatically with this launch file.",
+    )
+    
+    #   Simulation or Real
+    DeclareLaunchArgument(
+        "use_mock_hardware",
+        default_value="False",
+        description="Start robot with mock hardware mirroring command to its states.",
+    )
+
+    #   CAN Interface
+    DeclareLaunchArgument(
+        "can_interface",
+        default_value="can0",
+        description="CAN interface to use (e.g., 'can0' for real hardware, 'vcan0' for virtual).",
+    )
+
+    #   ROS 2 J1939 Static Bridge name
+    DeclareLaunchArgument(
+        "device_name",
+        default_value="umrt_ros_controller",
+        description="Name for the device using the static bridge.",
+    )
+
+    """
+    Variables
+    """
+    gui = LaunchConfiguration("gui")
+    use_mock_hardware = LaunchConfiguration("use_mock_hardware")
+    can_interface = LaunchConfiguration("can_interface")
+    device_name = LaunchConfiguration("device_name")
 
     """
     Nodes
     """
-    gps_node = Node(
-        package='gpsx',
-        executable='gps_node',
-        name='gps',
-        namespace='gps',
-        parameters=[{
-            'comm_port':"/dev/ttyUSB0",
-            'comm_speed': 4800
-        }]
-    )
-
     imu_node = Node(
         package='umrt-emb-imu-node',
         executable='umrt-emb-imu-node',
@@ -47,12 +85,54 @@ def generate_launch_description():
         namespace='imu'
     )
 
+    static_bridge_node = Node(
+        package='ros2_j1939_babbler',
+        executable='static_bridge',
+        name='static_bridge',
+        namespace='static_bridge',
+        parameters = [{
+            'device_name': device_name,
+            'can_interface': can_interface,
+            'device_ID': 0x80,
+        }]
+    )
+
     """
     External Launch Files
     """
+    #   Launch the Drivetrain System:
+    #       - ros2_j1939_babbler static_bridge
+    #       - ros2_socketcan sender and receiver
+    #       - drivetrain 
     drivetrain_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(drivetrain_launch_dir, 'diffbot.launch.py')
+            os.path.join(drivetrain_launch_dir, 'rover_drivetrain.launch.py')
+        ), 
+        launch_arguments={
+            'gui': gui, 
+            'use_mock_hardware': use_mock_hardware, 
+            'can_interface': can_interface
+        }.items()
+    )
+
+    ros2_socket_launch = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('ros2_socketcan'),
+                'launch',
+                'socket_can_bridge.launch.xml'
+            ])
+        ]),
+        launch_arguments={
+            'interface': can_interface,
+            'to_can_bus_topic': '/to_can_bus'
+        }.items()
+    )
+
+    # Launch the GPS Nodes, and Heading Node 
+    gps_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(gps_launch_dir, 'gps.launch.py')
         )
     )
 
@@ -61,10 +141,10 @@ def generate_launch_description():
     """
     rover = [
         drivetrain_launch,
-        imu_node,
-        gps_node
+        ros2_socket_launch,
+        static_bridge_node,
+        # imu_node,
+        # gps_launch
     ]
 
-    launch_entities = rover
-
-    return LaunchDescription(launch_entities)
+    return LaunchDescription(rover)
