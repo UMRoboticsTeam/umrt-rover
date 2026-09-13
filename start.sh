91ccbd48093f63496568c6eca75dea0599d0967f
domenic-chao <domenic.chao.emails@gmail.com>
2026-09-13 00:32:02 -0500

    Update start.sh
    Adding while for PID checking from mobile.

--- start.sh
+++ start.sh
@@ -30,3 +30,17 @@ export RES_CONFIG_DIR=$(realpath "${CONFIG_DIR}")
 
 # Starting Docker Container
 docker compose -f "./compose-rover.yaml" up -d --force-recreate --renew-anon-volumes
+
+sleep 2
+
+PID=$(docker inspect -f '{{.State.Pid}}' bridge_rover 2>/dev/null)
+while [ -z "$PID" ] || [ "$PID" -eq 0 ]; do
+    sleep 1
+    PID=$(docker inspect -f '{{.State.Pid}}' bridge_rover 2>/dev/null)
+done
+
+ip link set can1 down
+ip link set can1 netns "$PID"
+
+docker exec bridge_rover ip link set can1 up type can bitrate 500000
+
